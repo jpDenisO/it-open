228502bf0ea84f97e540730d4f5ce400a93c49e5
Juan Pablo Denis <juan.otarola@project-a.com>
2019-11-15 15:06:00 +0100

    files move

--- dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateTodoDto {
-  readonly name: string;
-  readonly priority: number;
-  readonly description: string;
-  readonly done: boolean;
-}
--- server/src/server/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TodosController } from './todos/todos.controller';
-
-@Module({
-  imports: [],
-  controllers: [TodosController],
-  providers: [],
-})
-export class AppModule {}
